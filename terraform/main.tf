113b2a9276a403d3dc5dec656065ad1223c977a1
viks1102 <viks_1102@yahoo.com>
2024-05-26 20:40:40 +0100

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-resource "google_storage_bucket" "gcs-bucket" {
-  name          = "bucket-with-rand-val-for-life"
-  location      = "us-east1"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-} 
+# resource "google_storage_bucket" "gcs-bucket" {
+#   name          = "bucket-with-rand-val-for-life"
+#   location      = "us-east1"
+#   storage_class = "STANDARD"
+#   versioning {
+#     enabled = true
+#   }
+# } 
